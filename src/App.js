a8e9fc0888fc778349c0a34f8b7347a897af403a
Barak Goren <barak.goren6@gmail.com>
2024-05-07 15:52:24 +0300

    init homework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Header1 from "./components/Header1";
+import Vip from "./pages/Vip";
+import VipInfo from "./pages/VipInfo";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      React 
-    </div>
+
+    <BrowserRouter>
+
+        <Header1 />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/vip" element={<Vip />} />
+          <Route path="/vip/:rank" element={<VipInfo />} />
+        </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
