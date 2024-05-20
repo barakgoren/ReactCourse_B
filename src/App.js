1a6d4b78dc06c539067fe209fe04877ec5dd19b7
Barak Goren <barak.goren6@gmail.com>
2024-05-20 13:21:55 +0300

    main branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Header1 from "./components/Header1";
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      React 
-    </div>
+
+    <BrowserRouter>
+
+        <Header1 />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+
+        </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
